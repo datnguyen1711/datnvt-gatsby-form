a591d2b9473dc36307d592f260536595cbb962bd
Nguyen Van Thanh Dat <dat.nguyen@madison-technology.com>
2022-03-18 16:13:15 +0700

    Update event click

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import {
   Button,
   VStack,
@@ -12,8 +12,26 @@ import {
 import { MdOutlineEmail } from "react-icons/md";
 import { BsPerson } from "react-icons/bs";
 import "../style/style.css";
+import { addJob, initState, reducer } from "../pages";
 
-export default function ContactForm() {
+export default function ContactForm({ setValueForm }: any) {
+  const [state, dispatch]: any = useReducer<any>(reducer, initState);
+  const [name, setName] = useState<string>("");
+  const [mail, setMail] = useState<string>("");
+  const [mess, setMess] = useState<string>("");
+
+  const handleInputName = (event: any) => {
+    setName(event.target.value);
+  };
+  const handleInputMail = (event: any) => {
+    setMail(event.target.value);
+  };
+  const handleInputMess = (event: any) => {
+    setMess(event.target.value);
+  };
+  const handleClick = () => {
+    dispatch(addJob({ name: name, mail: mail, mess: mess }));
+  };
   return (
     <div className="box-form">
       <div className="box-form-main">
@@ -25,7 +43,12 @@ export default function ContactForm() {
                 pointerEvents="none"
                 children={<BsPerson color="gray.800" />}
               />
-              <Input type="text" size="md" />
+              <Input
+                type="text"
+                size="md"
+                value={name}
+                onChange={handleInputName}
+              />
             </InputGroup>
           </FormControl>
           <FormControl id="name">
@@ -35,7 +58,12 @@ export default function ContactForm() {
                 pointerEvents="none"
                 children={<MdOutlineEmail color="gray.800" />}
               />
-              <Input type="text" size="md" />
+              <Input
+                type="text"
+                size="md"
+                value={mail}
+                onChange={handleInputMail}
+              />
             </InputGroup>
           </FormControl>
           <FormControl id="name">
@@ -46,10 +74,18 @@ export default function ContactForm() {
                 borderRadius: "gray.300",
               }}
               placeholder="message"
+              value={mess}
+              onChange={handleInputMess}
             />
           </FormControl>
           <FormControl id="name" float="right">
-            <Button variant="solid" bg="#0D74FF" color="white" _hover={{}}>
+            <Button
+              variant="solid"
+              bg="#0D74FF"
+              color="white"
+              _hover={{}}
+              onClick={handleClick}
+            >
               Send Message
             </Button>
           </FormControl>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,78 @@
-import React from "react";
-import { Box, Wrap } from "@chakra-ui/react";
+import React, { useReducer, useState } from "react";
+import {
+  Box,
+  Wrap,
+  Button,
+  VStack,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+} from "@chakra-ui/react";
 import "../style/style.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsPerson } from "react-icons/bs";
 import ContactForm from "../components/ContactForm";
 import ContactInfo from "../components/ContactInfo";
 
+export const initState = {
+  person: "",
+  persons: [],
+};
+
+const SET_PERSON = "SET_PERSON";
+const ADD_PERSON = "ADD_PERSON";
+
+export const addJob = (payload: any) => {
+  return {
+    type: ADD_PERSON,
+    payload,
+  };
+};
+
+export const reducer = (state: any, action: any) => {
+  let newState;
+  switch (action.type) {
+    case SET_PERSON:
+      newState = {
+        ...state,
+        person: action.payload,
+      };
+      break;
+    case ADD_PERSON:
+      newState = {
+        ...state,
+        persons: [...state.persons, action.payload],
+      };
+      break;
+    default:
+      throw new Error("Invalid action.");
+  }
+  return newState;
+};
+
 const IndexPage = () => {
+  const [name, setName] = useState<string>("");
+  const [mail, setMail] = useState<string>("");
+  const [mess, setMess] = useState<string>("");
+  const [state, dispatch]: any = useReducer<any>(reducer, initState);
+  const { person, persons }: any = state;
+
+  const handleInputName = (event: any) => {
+    setName(event.target.value);
+  };
+  const handleInputMail = (event: any) => {
+    setMail(event.target.value);
+  };
+  const handleInputMess = (event: any) => {
+    setMess(event.target.value);
+  };
+
+  const handleClick = () => {
+    dispatch(addJob({ name: name, mail: mail, mess: mess }));
+  };
   return (
     <div className="container">
       <Box
@@ -17,7 +85,87 @@ const IndexPage = () => {
         <div className="box-main">
           <div className="box-content">
             <ContactInfo />
-            <ContactForm />
+            <div className="box-form">
+              <div className="box-form-main">
+                <VStack spacing={5}>
+                  <FormControl id="name">
+                    <FormLabel>Your Name</FormLabel>
+                    <InputGroup borderColor="#E0E1E7">
+                      <InputLeftElement
+                        pointerEvents="none"
+                        children={<BsPerson color="gray.800" />}
+                      />
+                      <Input
+                        type="text"
+                        size="md"
+                        value={name}
+                        onChange={handleInputName}
+                      />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id="name">
+                    <FormLabel>Mail</FormLabel>
+                    <InputGroup borderColor="#E0E1E7">
+                      <InputLeftElement
+                        pointerEvents="none"
+                        children={<MdOutlineEmail color="gray.800" />}
+                      />
+                      <Input
+                        type="text"
+                        size="md"
+                        value={mail}
+                        onChange={handleInputMail}
+                      />
+                    </InputGroup>
+                  </FormControl>
+                  <FormControl id="name">
+                    <FormLabel>Message</FormLabel>
+                    <Textarea
+                      borderColor="gray.300"
+                      _hover={{
+                        borderRadius: "gray.300",
+                      }}
+                      placeholder="message"
+                      value={mess}
+                      onChange={handleInputMess}
+                    />
+                  </FormControl>
+                  <FormControl id="name" float="right">
+                    <Button
+                      variant="solid"
+                      bg="#0D74FF"
+                      color="white"
+                      _hover={{}}
+                      onClick={handleClick}
+                    >
+                      Send Message
+                    </Button>
+                  </FormControl>
+                </VStack>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Box>
+      <Box
+        bg="#02054B"
+        color="white"
+        borderRadius="lg"
+        m={{ sm: 4, md: 16, lg: 10 }}
+        p={{ sm: 5, md: 5, lg: 8 }}
+      >
+        <div className="box-title">List contact</div>
+        <div className="box-lists">
+          <div className="box-list">
+            {persons.map((item: any, index: number) => {
+              return (
+                <div className="box-item" key={index}>
+                  <div className="box-name">{item.name}</div>
+                  <div className="box-mail">{item.mail}</div>
+                  <div className="box-mess">{item.mess}</div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </Box>
